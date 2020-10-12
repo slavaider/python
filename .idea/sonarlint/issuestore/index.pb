99c031411674facd5f4abbb57c4c523646d4cdd5
slavaider <slavaider1@gmail.com>
2020-10-12 17:53:50 +0500

    end of web_python book

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,29 @@
+
+L
+repeat_from_book/01_hello.py,7\1\7100cbeee0cdd913b4fc67d3a51de9d354d0e1fe
+L
+repeat_from_book/02_beers.py,a\9\a90b82cb6b11679b2596475bea5ee9daadecda5b
+Q
+!repeat_from_book/04_dont_panic.py,9\3\9336b1e8bec4c88a4888375bdb36e0f8370ec8b2
+T
+$repeat_from_book/05_dont_panic_v2.py,1\2\12b3c0840de2982d0c8234e795622004620aaedd
+T
+$repeat_from_book/06_dont_panic_v3.py,e\3\e3eb95692ba3dd142368b973949a6b7e258ae639
+M
+repeat_from_book/07_marvin.py,c\c\cceae318d1cd75afa448504dc3963ddae3107eb9
+P
+ repeat_from_book/08_marvin_v2.py,4\e\4e10294a45b8fe8715fedba8e8925547b566e071
+L
+repeat_from_book/09_dicts.py,5\d\5da456b46b198128a1e68b04bec0e92e8516fb54
+d
+4repeat_from_book/web_development/templates/base.html,3\2\32a6eb099bec477443c2fc1b96d06fdf31838cd8
+^
+.repeat_from_book/web_development/static/hf.css,2\f\2fb3d9e177343d4d98b5eda091c3e1f87f257bc1
+K
+.idea/dictionaries/User.xml,1\5\15e64ecf26e71014d7e45c7fb5e7baa2dfbafc1e
+A
+files/vsearch.log,1\0\103cb78255a5014a2b7f7daaf70437a3b81179b7
+_
+/repeat_from_book/web_development/vsearch4web.py,3\9\39a22b4161838675e25c70f3f1a4b43061d6998e
+O
+repeat_from_book/03_list_dic.py,e\f\ef23c21afb2dbfd2b99367eef1ecb285422d5923
--- repeat_from_book/web_development/vsearch4web.py
+++ repeat_from_book/web_development/vsearch4web.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, escape
+from flask import Flask, render_template, request, redirect, escape
+import psycopg2
 
 
 def search4letters(phrase: str, letters: str = 'aeiou') -> set:
@@ -6,9 +7,13 @@ def search4letters(phrase: str, letters: str = 'aeiou') -> set:
     return set(letters).intersection(set(phrase))
 
 
-def log_request(req, res: str) -> None:
-    with open('../../files/vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+def log_request(req, results: str) -> None:
+    with connection:
+        with connection.cursor() as cursor:
+            _SQL = """INSERT INTO log(phrase, letters, ip, browser_string, results) VALUES (%s,%s,%s,%s,%s)"""
+            agent = str(req.user_agent.browser)
+            cursor.execute(_SQL, (req.form['phrase'], req.form['letters'], req.remote_addr, agent, results))
+            connection.commit()
 
 
 app = Flask(__name__)
@@ -33,15 +38,16 @@ def entry_page():
 
 @app.route('/view_log')
 def view_log():
-    contents = []
-    with open('../../files/vsearch.log') as log:
-        for i in log:
-            contents.append([])
-            for j in i.split('|'):
-                contents[-1].append(escape(j))
-    titles = ['Form Data', 'Remote_addr', 'User_agent', 'Results']
+    with connection:
+        with connection.cursor() as cursor:
+            _SQL = """SELECT id,ts,phrase,letters,ip,browser_string,results FROM log"""
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()
+            connection.commit()
+    titles = ['ID', 'Time', 'Phrase', 'Letters', 'IP', 'User_agent', 'Results']
     return render_template('view_log.html', the_title='View Log', row_titles=titles, the_data=contents)
 
 
 if __name__ == '__main__':
+    connection = psycopg2.connect(dbname='test_database', user='postgres', password='admin')
     app.run(debug=True)
