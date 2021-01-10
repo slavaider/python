0f85e3d1b6876f795b5c6384ed3a5b6cb0cbaff7
slavaider <slavaider1@gmail.com>
2021-01-10 17:11:03 +0500

    init

--- Machine Learning A-Z (Codes and Datasets)/Part 5 - Association Rule Learning/Section 29 - Eclat/Python/eclat.py
+++ Machine Learning A-Z (Codes and Datasets)/Part 5 - Association Rule Learning/Section 29 - Eclat/Python/eclat.py
@@ -3,19 +3,19 @@
 # Run the following command in the terminal to install the apyori package: pip install apyori
 
 # Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 # Data Preprocessing
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
 transactions = []
 for i in range(0, 7501):
-  transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
+    transactions.append([str(dataset.values[i, j]) for j in range(0, 20)])
 
 # Training the Eclat model on the dataset
 from apyori import apriori
-rules = apriori(transactions = transactions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2, max_length = 2)
+
+rules = apriori(transactions=transactions, min_support=0.003, min_confidence=0.2, min_lift=3, min_length=2,
+                max_length=2)
 
 # Visualising the results
 
@@ -23,13 +23,16 @@ rules = apriori(transactions = transactions, min_support = 0.003, min_confidence
 results = list(rules)
 print(results)
 
+
 ## Putting the results well organised into a Pandas DataFrame
 def inspect(results):
-    lhs         = [tuple(result[2][0][0])[0] for result in results]
-    rhs         = [tuple(result[2][0][1])[0] for result in results]
-    supports    = [result[1] for result in results]
+    lhs = [tuple(result[2][0][0])[0] for result in results]
+    rhs = [tuple(result[2][0][1])[0] for result in results]
+    supports = [result[1] for result in results]
     return list(zip(lhs, rhs, supports))
-resultsinDataFrame = pd.DataFrame(inspect(results), columns = ['Product 1', 'Product 2', 'Support'])
+
+
+resultsinDataFrame = pd.DataFrame(inspect(results), columns=['Product 1', 'Product 2', 'Support'])
 
 ## Displaying the results sorted by descending supports
-resultsinDataFrame.nlargest(n = 10, columns = 'Support')
+resultsinDataFrame.nlargest(n=10, columns='Support')
